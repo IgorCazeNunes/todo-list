713215f78d28befc747c9fed6494e074901a6ef1
Igor Caze <igorcaze@gmail.com>
2022-10-08 22:21:07 -0300

    chore: padronize archives spaces;

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,29 @@ import { Form, TodoList } from './components';
 import './App.css';
 
 const App = () => {
-  const [todoList, setTodoList] = useState<ITodo[]>(() => JSON.parse(localStorage.getItem(`@Todo-List`) || "[]"));
-  const [totalItems, setTotalItems] = useState(0);
+	const [todoList, setTodoList] = useState<ITodo[]>(() => JSON.parse(localStorage.getItem(`@Todo-List`) || "[]"));
+	const [totalItems, setTotalItems] = useState(0);
 
-  const handleSaveLocal = (todoList: ITodo[]) => {
-    localStorage.setItem(`@Todo-List`, JSON.stringify(todoList));
-  }
+	const handleSaveLocal = (todoList: ITodo[]) => {
+		localStorage.setItem(`@Todo-List`, JSON.stringify(todoList));
+	}
 
-  useEffect(() => {
-    setTotalItems(todoList.length);
-    handleSaveLocal(todoList);
-  }, [todoList]);
+	useEffect(() => {
+		setTotalItems(todoList.length);
+		handleSaveLocal(todoList);
+	}, [todoList]);
 
-  return (
-    <main className='App'>
-      <h1>to-do list</h1>
+	return (
+		<main className='App'>
+			<h1>to-do list</h1>
 
-      <div>
-        <Form todoList={todoList} setTodoList={setTodoList} />
+			<div>
+				<Form todoList={todoList} setTodoList={setTodoList} />
 
-        <TodoList todoList={todoList} setTodoList={setTodoList} totalItems={totalItems} />
-      </div>
-    </main>
-  );
+				<TodoList todoList={todoList} setTodoList={setTodoList} totalItems={totalItems} />
+			</div>
+		</main>
+	);
 }
 
 export default App;
