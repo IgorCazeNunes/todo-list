fdc3e9d3498bd325784e12704f1bad715f64a04b
Igor Caze <igorcaze@gmail.com>
2022-10-08 22:38:38 -0300

    chore: create Home page.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import { ITodo } from '../../common/interfaces';
+import { Form, TodoList } from '../../components';
+import { Header } from '../../layouts';
+
+import './index.css';
+
+const Home = () => {
+	const [todoList, setTodoList] = useState<ITodo[]>(() => JSON.parse(localStorage.getItem(`@Todo-List`) || "[]"));
+	const [totalItems, setTotalItems] = useState(0);
+
+	const handleSaveLocal = (todoList: ITodo[]) => {
+		localStorage.setItem(`@Todo-List`, JSON.stringify(todoList));
+	}
+
+	useEffect(() => {
+		setTotalItems(todoList.length);
+		handleSaveLocal(todoList);
+	}, [todoList]);
+
+	return (
+		<main className='App'>
+			<Header />
+
+			<div>
+				<Form todoList={todoList} setTodoList={setTodoList} />
+
+				<TodoList todoList={todoList} setTodoList={setTodoList} totalItems={totalItems} />
+			</div>
+		</main>
+	);
+}
+
+export default Home;
