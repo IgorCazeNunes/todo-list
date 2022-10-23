c0bbe95875c440b6780c98b0ed57a97392fac8db
Igor Caze <igorcaze@gmail.com>
2022-10-23 09:13:56 -0300

    chore: abstract header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
+import Header from './layouts/Header';
+
 import './reset.css';
 import './app.css';
 
 const App = () => (
     <main className='App'>
         {/* Header */}
-        <header>
-            <h1>React Todo List</h1>
-        </header>
+        <Header />
         {/* Header */}
 
         {/* Home */}
---
+++ src/layouts/Header/index.jsx
@@ -0,0 +1,7 @@
+const Header = () => (
+    <header>
+        <h1>React Todo List</h1>
+    </header>
+);
+
+export default Header;
